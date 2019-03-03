a342c575a2c6ee94979520cdb1dab3889919a7b4
Giancarlos <giancarlos.isasi@gmail.com>
2019-03-02 20:02:20 -0500

    Create tag model

--- app/core/__test__/test_models.py
+++ app/core/__test__/test_models.py
@@ -1,6 +1,16 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def create_sample_user(email='test@gmail.com', password='testpassword'):
+    '''Create a sample user model'''
+    return get_user_model().objects.create_user(
+        email=email,
+        password=password,
+    )
+
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
@@ -36,3 +46,10 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        tag = models.Tag.objects.create(
+            user=create_sample_user(),
+            name='Vegan',
+        )
+        self.assertEqual(str(tag), tag.name)
